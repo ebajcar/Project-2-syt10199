75e1df7f8b3df1ce25efd6c9c569fed07940453f
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2020-06-29 13:38:36 -0400

    Update ttt_oo_skeleton.js

--- ttt_oo_skeleton.js
+++ ttt_oo_skeleton.js
@@ -35,7 +35,7 @@ Description:
 var Game = function () {
 	this.player = "X";
 	this.winSets = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],
-					        [1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
+			[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
 	this.empty = 9;
 	this.gameover = false;
 	this.board = document.getElementsByTagName("td");
